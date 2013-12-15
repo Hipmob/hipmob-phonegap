6e9afcbc86545fe7e3f0034f85d0190706c959da
Femi Omojola <femi@hipmob.com>
2013-12-15 13:54:20 -0500

    Removed the bin/gen files from git

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\projects\WanderPlayer\hipmob\apps\hipmob-phonegap-test\HipmobGap\platforms\android\bin\AndroidManifest.xml : \
-C:\projects\WanderPlayer\hipmob\apps\hipmob-phonegap-test\HipmobGap\platforms\android\AndroidManifest.xml \
